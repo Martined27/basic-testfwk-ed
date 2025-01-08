cb898e5c8faf036164baadf5f1e0e02bd1708b06
martined <edmundodeleon@gmail.com>
2025-01-07 21:08:49 -0400

    Adding reports + adding loggers

--- tests/comments/test_comments.py
+++ tests/comments/test_comments.py
@@ -1,9 +1,10 @@
 import pytest
-from config import APP_URL
+from config import APP_URL, LOG
 from lib.comments import Comments
 
 
-
 def test_get_all_comments(login_as_admin):
+    LOG.info("test_get_all_comments")
     response = Comments().get_all_comments(APP_URL, login_as_admin)
+    LOG.info(response.json())
     assert response.ok
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
-from config import SESSION,APP_URL, ADMIN_USERNAME, ADMIN_PASSWORD
+from config import SESSION,APP_URL, ADMIN_USERNAME, ADMIN_PASSWORD, LOG
 
 
 @pytest.fixture(scope="session")
 def login_as_admin():
-    response =  SESSION.post(f"{APP_URL}/auth/login", data={"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD})
+    LOG.info("Logging in as admin")
+    payload = {"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD}
+    LOG.debug(f"Payload: %s, {payload}")
+    response =  SESSION.post(f"{APP_URL}/auth/login", data=payload)
     assert response.ok
     access_token = response.json()["access_token"]
     return access_token
