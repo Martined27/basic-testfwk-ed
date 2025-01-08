eed24940df9b3288309c44423c1a6dc75849c31a
martined <edmundodeleon@gmail.com>
2025-01-07 21:08:49 -0400

    Adding reports + adding loggers

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
+import logging
 import requests
 
 SESSION = requests.Session()
 APP_URL = "http://localhost:8080"
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin"
+
+LOG = logging.getLogger()
+
+class HideSensitiveData(logging.Filter):
+    def filter(self, record):
+       record.msg = str(record.msg).replace(ADMIN_PASSWORD, "*******")
+       return True
+    
+LOG.addFilter(HideSensitiveData())
+     
