f8a78337ea1f474bce306339af1e5eb8c1b72f40
martined <edmundodeleon@gmail.com>
2025-01-13 19:04:09 -0400

    Adding auth file to use on users workflow

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,15 @@
-def build_request_headers(access_token, content_type="application/json"):
+from config import LOG
+
+
+def build_request_headers(access_token, content_type="application/json", **kwargs):
     headers = {
         "Authorization": "Bearer " + access_token,
         "Accept": content_type
     }
+    if "content_type" in kwargs:
+        headers["Content-Type"] = kwargs["content_type"]
+
+
+    LOG.debug(f"Request headers: {headers}")
+    
     return headers
