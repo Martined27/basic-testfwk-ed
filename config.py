72180ce32a8f4f1236e9ae1890d68902e4c7d1d7
martined <edmundodeleon@gmail.com>
2025-01-13 19:03:43 -0400

    Adding hiding auth bearer

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import requests
 
 SESSION = requests.Session()
@@ -11,6 +12,7 @@ LOG = logging.getLogger()
 class HideSensitiveData(logging.Filter):
     def filter(self, record):
        record.msg = str(record.msg).replace(ADMIN_PASSWORD, "*******")
+       record.msg = re.sub(r"Authorization: Bearer [a-zA-Z0-9]+", "Authorization: Bearer *******", record.msg)
        return True
     
 LOG.addFilter(HideSensitiveData())
